17b341977cf359e1fe43ae268a693c78327c3fd8
Victoria TOVIHOUANDE <tvhvictoria@gmail.com>
2026-07-17 01:35:02 +0200

    Suppression du fichier checking.py

--- src/ingestion/checking.py
+++
@@ -1,6 +0,0 @@
-with open("data/cleaned_corpus.csv", "r", encoding="utf-8") as f:
-    for i, ligne in enumerate(f, start=1):
-        mots = ligne.split()
-        for mot in mots:
-            if "đ" in mot:
-                print(i, mot)
